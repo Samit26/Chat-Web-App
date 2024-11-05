19c2d89c0ec27f3309564e8531271cf7969c817d
Samit26 <samitkhedekar2594@gmail.com>
2024-11-05 20:38:44 +0530

    vision

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: "production",
-    sameSite: "strict",
+    sameSite: "none",
     path: "/",
   });
 };
